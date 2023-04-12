f47a29ea03f7cbc2aa2707c739a2e35429fc3bc8
guilhemkzk <guilhem.l.krawczyk@gmail.com>
2023-04-12 13:19:17 +0200

    Get and display the tag list using Vanilla JS

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+//INPUTS
+
 let columns = {
   xs: 1,
   sm: 2,
@@ -6,11 +8,19 @@ let columns = {
   xl: 3,
 };
 
+let lightBox = true;
+let lightboxId = "myAwesomeLightbox";
+let showTags = true;
+let tagsPosition = "top";
+
+// ----------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------- //
+// #region --------------------------POPULATE GALLERY WITH IMAGES -------------------------- //
+// ----------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------- //
+
 // Get the container of the gallery of works
 let galleryContainer = document.getElementById("gallery-container");
-
-// Get a variable with all the images (HTML Collection)
-let imagesInGallery = document.getElementsByClassName("gallery-item");
 
 // General function to display the gallery
 async function displayGallery(location) {
@@ -19,13 +29,12 @@ async function displayGallery(location) {
   // location.style.opacity = "1"; // Effet fade in via CSS sur 5 secondes
 }
 
+// General function to add the Bootstrap classes to the images
 async function addBootstrapClasses(location, columns) {
   // Replace wrapItemInColumn
   // ==> add the classes in each image for bootstrap responsive design
 
   // Get a variable with all the images (HTML Collection) and convert it to an array
-  // In order to use .map() on it
-
   let imagesInGallery = document.getElementsByClassName("gallery-item");
   let imagesInGalleryArray = [];
 
@@ -51,6 +60,7 @@ async function addBootstrapClasses(location, columns) {
       }
     }
 
+    // Add the <div> around
     const returnImages =
       '<div class="gallery-items-row row">' +
       imagesInGalleryArray
@@ -72,5 +82,75 @@ async function addBootstrapClasses(location, columns) {
 }
 
 displayGallery(galleryContainer);
-
 addBootstrapClasses(galleryContainer, columns);
+
+// #endregion
+
+// ----------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------- //
+// #region --------------------------CREATING THE FILTERING TAGS --------------------------- //
+// ----------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------- //
+
+// General function to get all the tags from HTML images
+async function displayAllTags(location) {
+  //
+  // GET THE TAGS FROM THE HTML
+  //
+
+  // Initiate the variables
+  let tagsCollection = [];
+
+  // Get a variable with all the images (HTML Collection)
+  let imagesInGallery = document.getElementsByClassName("gallery-item");
+
+  // Iterate on imagesInGallery to get all the tags
+  let counter = 0;
+  for (let i = 0; i < imagesInGallery.length; i++) {
+    if (!tagsCollection.includes(imagesInGallery[i].attributes.tag.nodeValue)) {
+      tagsCollection[counter] = imagesInGallery[i].attributes.tag.nodeValue;
+      counter++;
+    }
+  }
+
+  //Add the "tous" tag at the begining of the array
+  tagsCollection.unshift("Tous"); // TYPE = OBJECT
+
+  //
+  // DISPLAY THE TAGS IN A LIST
+  //
+
+  // Turn tagsCollection to an array
+  let tagsCollectionArray = [];
+
+  for (let i = 0; i < tagsCollection.length; i++) {
+    tagsCollectionArray[i] = tagsCollection[i];
+  }
+
+  const returnTagList =
+    '<ul class="my-4 tags-bar nav nav-pills">' +
+    tagsCollectionArray
+      .map(
+        (tagsCollectionArray) => `
+  <li class="nav-item active">
+  <span class="nav-link"  data-images-toggle="${tagsCollectionArray}">${tagsCollectionArray}</span></li>`
+      )
+      .join("") +
+    "</ul>";
+
+  if (showTags) {
+    // IF the position (defined by user) is BOTTOM, display at the bottom
+    if (tagsPosition === "bottom") {
+      location.insertAdjacentHTML("beforeend", returnTagList); // on ajoute la liste des tags après la gallerie
+    } else if (tagsPosition === "top") {
+      // IF the position is TOP
+      location.insertAdjacentHTML("afterbegin", returnTagList); // on ajoute la liste des tags avant la gallerie
+    } else {
+      console.error(`Unknown tags position: ${tagsPosition}`); // sinon on renvoie une erreur
+    }
+  }
+}
+
+displayAllTags(galleryContainer);
+
+// #endregion
